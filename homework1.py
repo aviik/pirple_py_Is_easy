a2508d584b7a2971aceb738d6a753bfe0c2b3ae9
aviik <mailforavik@hotmail.com>
2018-12-24 19:10:11 +0530

    variables for printing song attributes added

---
+++ homework1.py
@@ -0,0 +1,25 @@
+""" Program to enlist some of the important 
+                attributes that could describe a song """
+
+
+#Attribute variables
+song = "  I'll Play The Blues For You"
+artist = "Daniel Castro"
+albumn = "No Surrender"
+year = 1999
+genre = "Blues"
+sub_genre = "Blues-Contemporary"
+track_number = 8
+song_duration = 7.42
+
+#Attribute Labels Print Format
+print("")
+print("")
+print("Song:      ", song)
+print("Artist:      ", artist)
+print("Albumn:      ", albumn)
+print("Year:        ", year)
+print("Genre:       ", genre)
+print("Sub-genre:   ", sub_genre)
+print("Track Number:",track_number)
+print("Length:      ", song_duration)
